a68bf8cb5a4285ce4c856a259436add767b4c88c
Yash Bansod <yashbansod33@gmail.com>
2020-11-24 13:39:05 +0000

    updated grp

--- Server.c
+++ Server.c
@@ -164,10 +164,10 @@ char* cli(struct userinfo * user, char *inbuf, char *sndbuf) {
 	else if(user->mode == 4) {
 		int t = inbuf[9] - '0';
 		strcpy(user->tmid, users[t-1].username);
-		user->mode = 0;
+		user->mode = 3;
 		strcpy(sndbuf, user->tmid);
 		strcat(sndbuf, "is your teammate\n");
-		strcpy(sndbuf, "PRESS ENTER TO CONTINUE\n");
+		strcpy(sndbuf, "PRESS n TO CONTINUE\n");
 		return sndbuf;
 	}
 
